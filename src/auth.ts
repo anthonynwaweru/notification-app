3ed9987a20313847c1521e92295bd0ed3612d2fd
anthonynwaweru <anthonypmarcel@gmail.com>
2024-03-13 20:11:35 +0100

    implementing apis

--- src/auth.ts
+++ src/auth.ts
@@ -7,5 +7,29 @@ const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-  throw new Error();
+  throw new Error("Missing github oauth credentials");
 }
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signOut,
+  signIn,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    // usually not necessary but fixing a bug in nextauth
+    async session({ session, user }: any) {
+      if (session && user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
