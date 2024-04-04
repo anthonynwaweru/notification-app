1606c846513d22d7257b8559e57316bbbe1ea116
anthonynwaweru <anthonypmarcel@gmail.com>
2024-04-04 09:39:18 +0200

    formData implementation & inline-error Handling

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,37 @@
 "use server";
+import { error } from "console";
+import { z } from "zod";
 
-export async function createTopic() {
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form: string[];
+  };
+}
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const name = formData.get("name");
+  const description = formData.get("description");
+  console.log(name, description);
+  const data = createTopicSchema.safeParse({ name, description });
+  if (!data.success) {
+    return { errors: data.error.flatten().fieldErrors };
+  }
+  return {
+    errors: {},
+  };
   // revalidate the homepage after creating a new topic
 }
